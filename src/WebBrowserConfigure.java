261b848f4fc914cb377c23aed9f62b7b9c5f098d
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2021-03-19 14:25:48 -0700

    Update WebBrowserConfigure.java

--- src/WebBrowserConfigure.java
+++ src/WebBrowserConfigure.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class WebBrowserConfigure {
@@ -6,17 +8,45 @@ public class WebBrowserConfigure {
         ClassLoader classLoader = WebBrowserConfigure.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream("Control.txt");
         String data = readFromInputStream(inputStream);
-        System.out.println(data);
+
+        Path fileName = Path.of("Control.txt");
+        Files.writeString(fileName, data);
+//        System.out.println(data);
+        //write to control file with seperated lines for choice and answer
     }
 
     private static String readFromInputStream(InputStream inputStream) throws IOException {
         StringBuilder resultStringBuilder = new StringBuilder();
+        String option = "";
+        String fileTxt = "";
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
+            String lane;
             while ((line = br.readLine()) != null) {
+                lane = br.readLine();
                 resultStringBuilder.append(line).append("\n");
+                lane = changeMe(line, lane);
+                // return the answer and update to lane
+                option += line + " is " + lane + "\n";
+                fileTxt += line + "\n" + lane + "\n";
             }
         }
-        return resultStringBuilder.toString();
+//        return resultStringBuilder.toString();
+        System.out.println(option);
+        return fileTxt;
+    }
+
+    private static String changeMe(String setting, String choice) {
+        System.out.println(setting + " is ...\n1. off\t2. on" );
+        Scanner myObj = new Scanner(System.in);
+        int ans = myObj.nextInt();
+        if(ans == 1) {
+            System.out.println("off\n\n");
+            return "off";
+        } else {
+            System.out.println("on\n\n");
+            return "on";
+        }
+//        return choice;
     }
 }

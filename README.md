c64320b66244cbc2d3a59eab78b3911d232f13ba
Abraham Calvillo <31970808+Abruuham@users.noreply.github.com>
2021-03-19 21:54:15 -0700

    Making changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OnionRouting
-sos
+This is the Onion router, created to allow secure browsing and the ability to excrypt text files.
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
